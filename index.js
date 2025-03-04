c14289b7ec21f07fcb39caf0ec091eed1e9b3412
VasKD <kiara.denisse96@gmail.com>
2025-03-04 11:27:11 -0500

    Refactor code

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function add(num1, num2) {
-     return (num1) + (num2);
+     return ((num1) + (num2)).toFixed(2);
 }
 
 function subtract(num1, num2) {
-    return (num1) - (num2);
+    return ((num1) - (num2)).toFixed(2);
 }
 
 function multiply(num1, num2) {
-    return (num1) * (num2);
+    return ((num1) * (num2)).toFixed(2);
 }
 
 function divide(num1, num2) {
     let quotient = (num1) / (num2);
-    return num2 !== 0 ? quotient.toPrecision(2) : alert("Error: Division by zero");
+    return num2 !== 0 ? quotient.toFixed(2) : alert("Error: Division by zero");
 }
 
 function operate(num1, operator, num2) {
@@ -31,110 +31,141 @@ function operate(num1, operator, num2) {
 }
 
 
-// Event listeners for buttons
+// Variables
 let display = document.getElementsByClassName("result");
 let expression = document.getElementsByClassName("expression");
 const buttons = document.querySelectorAll("button");
 let values = [];
+let operand = "";
+let negateNum = 0;
+let negated = false;
 
 
 function handleInput(input, negated = null) {
     if (negated) {
-        console.log("hello! this expression is negated");
         input = input.split(/(-?\d+\.\d+|-?\d+|\D)/).filter(num => num !== "");
-        console.log(input);
     } else {
         input = input.split(/(\d+\.\d+|\d+|\D)/).filter(num => num !== "");
-        console.log(input);
     }
-    
 
     let num1 = Number(input[0]);
     let operator = input[1];
     let num2 = Number(input[2]);
 
     let result = operate(num1, operator, num2);
-    display[0].textContent = result;
 
-    expression[0].textContent = `${num1} ${operator} ${num2} =`;
-    expression[0].style.color = "grey";
-
+    displayResult(result);
+    displayExpression(`${num1} ${operator} ${num2} =`);
     return result;
 }
 
-let negated = false;
-let operand = "";
 
-
-
-function handleNegate(num) {
-    console.log("hey, it's time to negate");
-    if (-num < 0) {
-        negated = true; 
-    } else {
-        negated = false;
-    }
-    console.log(num);
-    return -num;
+// display the result on the calculator
+function displayResult(result) {
+    display[0].textContent = result; 
 }
 
+// display the expression on the calculator
+function displayExpression(expr) {
+    expression[0].textContent = expr;
+    expression[0].style.color = "grey";
+}
 
+// event listeners
 buttons.forEach(button => {
     button.addEventListener("click", function() {
-        const value = button.getAttribute('data-value');
-        let result = 0;
-        let negateNum = 0;
-        if (['+', "-", "*", "/"].includes(value)) {
+        const value = button.getAttribute("data-value");
+        
+        if (["+", "-", "*", "/"].includes(value)) {
             operand = value;
             console.log(operand);
         }
         if (value === "negate") {
-            console.log("hey, it's time to negate this expression");
-            console.log(operand);
-            if (values.includes(operand)) {
-                console.log("get index of operand");
-                const index = values.indexOf(operand);
-                console.log(index);
-                let postOperandNum = values.splice(index+1, values.length-index).join('');
-                console.log(postOperandNum);
-                negateNum = handleNegate(postOperandNum);
-            } else {
-                console.log("nope, operand not in values");
-                console.log(values);
-                let preOperandNum = values.splice(0).join('');
-                negateNum = handleNegate(preOperandNum);
-
-            }
-            values.push(negateNum);
-            console.log(values)
-            display[0].textContent = values.join("");
+            handleNegateButton();
         } else if (value === "enter") {
-            console.log("hey, it's time to evaluate this expression");
-            const str = values.join("");
-            if (negated) {
-                values = [];
-                result = handleInput(str, negated);
-                values.push(result);
-                console.log(result);
-            } else {
-                values = [];
-                result = handleInput(str);
-                values.push(result);
-            }
-            console.log(result);
+            handleEnterButton();
         } else if (value === "clear") {
-            values = [];
-            console.log("hey, it's time to clear this expression");
-            display[0].textContent = "0";
-            expression[0].style.color = "#081c15";
+           handleClearButton();
         } else if (value === "delete") {
-            values.pop();
-            display[0].textContent = values.join("");
+            handleDeleteButton();
         } else {
-            values.push(value);
-            display[0].textContent = values.join("");
+            handleNumberButton(value);
         }
         console.log(values);
         console.log(negated);
     });
 });
+
+
+
+// handle button clicks
+function handleNegateButton() {
+    console.log("hey, it's time to negate this expression");
+    console.log(operand);
+    if (values.includes(operand)) {
+        console.log("get index of operand");
+        const index = values.indexOf(operand);
+        console.log(index);
+        let postOperandNum = values.splice(index+1, values.length-index).join('');
+        console.log(postOperandNum);
+        negateNum = negate(postOperandNum);
+    } else {
+        console.log("nope, operand not in values");
+        console.log(values);
+        let preOperandNum = values.splice(0).join('');
+        negateNum = negate(preOperandNum);
+    }
+    values.push(negateNum);
+    console.log(values)
+    displayResult(values.join(""));
+}
+
+// return negated number and indicate if it is negative
+function negate(num) {
+    console.log("hey, it's time to negate");
+    if (-num < 0) {
+        negated = true;
+    } else {
+        negated = false;
+    }
+    return -num;
+}
+
+function handleEnterButton() {
+    console.log("hey, it's time to evaluate this expression");
+    const expr = values.join("");
+    let result = 0;
+
+    if (negated) {
+        values = [];
+        result = handleInput(expr, negated);
+        values.push(result);
+        console.log(result);
+    } else {
+        values = [];
+        result = handleInput(expr);
+        values.push(result);
+    }
+
+    // console.log(result);
+    values = [];
+    values.push(result);
+}
+
+function handleClearButton() {
+    console.log("hey, it's time to clear this expression");
+    values = [];
+    displayContent("0");
+    expression[0].style.color = "#081c15";
+}
+
+function handleDeleteButton() {
+    console.log("hey, it's time to delete this expression");
+    values.pop();
+    displayContent(values.join(""));
+}
+
+function handleNumberButton(value) {
+    values.push(value);
+    displayResult(values.join(""));
+}
