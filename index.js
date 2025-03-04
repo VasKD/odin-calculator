90fff2a43cd5b2e2786d7f8b231d27a31a033a68
VasKD <kiara.denisse96@gmail.com>
2025-03-03 23:52:03 -0500

    Make negate button functional

--- index.js
+++ index.js
@@ -38,9 +38,16 @@ const buttons = document.querySelectorAll("button");
 let values = [];
 
 
-function handleInput(input) {
-    input = input.split(/(\d+\.\d+|\d+|\D)/).filter(num => num !== "");
-    console.log(input);
+function handleInput(input, negated = null) {
+    if (negated) {
+        console.log("hello! this expression is negated");
+        input = input.split(/(-?\d+\.\d+|-?\d+|\D)/).filter(num => num !== "");
+        console.log(input);
+    } else {
+        input = input.split(/(\d+\.\d+|\d+|\D)/).filter(num => num !== "");
+        console.log(input);
+    }
+    
 
     let num1 = Number(input[0]);
     let operator = input[1];
@@ -55,16 +62,66 @@ function handleInput(input) {
     return result;
 }
 
+let negated = false;
+let operand = "";
+
+
+
+function handleNegate(num) {
+    console.log("hey, it's time to negate");
+    if (-num < 0) {
+        negated = true; 
+    } else {
+        negated = false;
+    }
+    console.log(num);
+    return -num;
+}
+
+
 buttons.forEach(button => {
     button.addEventListener("click", function() {
         const value = button.getAttribute('data-value');
-        if (value === "enter") {
+        let result = 0;
+        let negateNum = 0;
+        if (['+', "-", "*", "/"].includes(value)) {
+            operand = value;
+            console.log(operand);
+        }
+        if (value === "negate") {
+            console.log("hey, it's time to negate this expression");
+            console.log(operand);
+            if (values.includes(operand)) {
+                console.log("get index of operand");
+                const index = values.indexOf(operand);
+                console.log(index);
+                let postOperandNum = values.splice(index+1, values.length-index).join('');
+                console.log(postOperandNum);
+                negateNum = handleNegate(postOperandNum);
+            } else {
+                console.log("nope, operand not in values");
+                console.log(values);
+                let preOperandNum = values.splice(0).join('');
+                negateNum = handleNegate(preOperandNum);
+
+            }
+            values.push(negateNum);
+            console.log(values)
+            display[0].textContent = values.join("");
+        } else if (value === "enter") {
             console.log("hey, it's time to evaluate this expression");
             const str = values.join("");
-            let result = handleInput(str);
+            if (negated) {
+                values = [];
+                result = handleInput(str, negated);
+                values.push(result);
+                console.log(result);
+            } else {
+                values = [];
+                result = handleInput(str);
+                values.push(result);
+            }
             console.log(result);
-            values = [];
-            values.push(result);
         } else if (value === "clear") {
             values = [];
             console.log("hey, it's time to clear this expression");
@@ -78,5 +135,6 @@ buttons.forEach(button => {
             display[0].textContent = values.join("");
         }
         console.log(values);
+        console.log(negated);
     });
 });
